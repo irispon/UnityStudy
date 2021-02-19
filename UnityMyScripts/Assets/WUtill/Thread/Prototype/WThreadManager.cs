fb5e8de4411fa768eed6a9440b558202e373b397
신원우 <ptlsdnjsdnp@gmail.com>
2021-02-19 18:54:58 +0900

    멀티 스레딩 프로토타입 구현중

---
+++ UnityMyScripts/Assets/WUtill/Thread/Prototype/WThreadManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace prototype
+{
+    //타임 스레드
+    //블록킹 스레드
+    public class WThreadManager  : MonoBehaviour
+    {
+        private Queue<Action> longTasks;
+        private Queue<Action> shortTasks;
+        private Task[] threads;
+        private bool enableThread;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Init();
+        }
+        void Init()
+        {
+            longTasks = new Queue<Action>();
+            shortTasks = new Queue<Action>();
+            if (Environment.ProcessorCount > 3)
+            {
+                enableThread = true;
+                threads = new Task[(int)(Environment.ProcessorCount*0.75)];
+            }
+            else
+            {
+                enableThread = false;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+
+    }
+
+}
+
