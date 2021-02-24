0b4b44d9fc4ccf160fc189aceb2f6cdb6e353201
신원우 <ptlsdnjsdnp@gmail.com>
2021-02-24 17:54:07 +0900

    Thread매니저 추가 내용

--- UnityMyScripts/Assets/WUtill/Thread/Prototype/WThreadManager.cs
+++ UnityMyScripts/Assets/WUtill/Thread/Prototype/WThreadManager.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
-namespace prototype
+namespace WThread
 {
     //타임 스레드
     //블록킹 스레드
@@ -12,28 +15,47 @@ namespace prototype
     {
         private Queue<Action> longTasks;
         private Queue<Action> shortTasks;
-        private Task[] threads;
-        private bool enableThread;
-
+        public static Queue<WorkerThread> threadQue;
+        public static WThreadManager GET { get ; private set; }
         // Start is called before the first frame update
+        private void Awake()
+        {
+
+            if (GET == null)
+                GET = this;
+            else
+                Destroy(this);
+
+            threadQue = new Queue<WorkerThread>();
+        }
+        public void EnQueThread(WorkerThread thread)
+        {
+            thread.action = null;
+            thread.onUIThread = null;
+            threadQue.Enqueue(thread);
+
+        }
+        public WorkerThread DeQueThread(Action work =null,Action ui=null)
+        {
+            WorkerThread thread=null;
+            if (threadQue.Count > 0)
+            {
+                thread = threadQue.Dequeue();
+                thread.action = work;
+                thread.onUIThread = ui;
+            }
+
+            return thread;
+        }
         void Start()
         {
+           
             Init();
         }
         void Init()
         {
             longTasks = new Queue<Action>();
             shortTasks = new Queue<Action>();
-            if (Environment.ProcessorCount > 3)
-            {
-                enableThread = true;
-                threads = new Task[(int)(Environment.ProcessorCount*0.75)];
-                Debug.Log((int)(Environment.ProcessorCount * 0.75));
-            }
-            else
-            {
-                enableThread = false;
-            }
         }
 
         // Update is called once per frame
